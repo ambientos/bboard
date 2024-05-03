6bddc49bf00ca9d2d42d746c8436b79d0eec326d
Anton Grakhov <ant.grakhov@gmail.com>
2024-05-03 04:05:59 +0300

    Add validation

--- resources/views/bb-create.blade.php
+++ resources/views/bb-create.blade.php
@@ -7,14 +7,25 @@
         @csrf
         <div class="mb-3">
             <label for="txtTitle" class="form-label">Товар</label>
-            <input name="title" id="txtTitle" class="form-control"> </div>
+            <input name="title" id="txtTitle" class="form-control @error('title') is-invalid @enderror">
+            @error('title')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
         <div class="mb-3">
             <label for="txtContent" class="form-label">Описание</label>
-            <textarea name="content" id="txtContent" class="form-control" row="3"></textarea>
+            <textarea name="content" id="txtContent" class="form-control @error('content') is-invalid @enderror" row="3"></textarea>
+            @error('content')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="txtPrice" class="form-label">Цена</label>
-            <input name="price" id="txtPrice" class="form-control"> </div>
+            <input name="price" id="txtPrice" class="form-control @error('price') is-invalid @enderror">
+            @error('price')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
         <input type="submit" class="btn btn-primary" value="Добавить">
     </form>
 @endsection('content')
--- resources/views/bb-edit.blade.php
+++ resources/views/bb-edit.blade.php
@@ -8,15 +8,24 @@
         @method('PATCH')
         <div class="mb-3">
             <label for="txtTitle" class="form-label">Товар</label>
-            <input name="title" id="txtTitle" class="form-control" value="{{$bb->title}}">
+            <input name="title" id="txtTitle" class="form-control @error('title') is-invalid @enderror" value="{{$bb->title}}">
+            @error('title')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="txtContent" class="form-label">Описание</label>
-            <textarea name="content" id="txtContent" class="form-control" row="3">{{$bb->content}}</textarea>
+            <textarea name="content" id="txtContent" class="form-control @error('content') is-invalid @enderror" row="3">{{$bb->content}}</textarea>
+            @error('content')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="txtPrice" class="form-label">Цена</label>
-            <input name="price" id="txtPrice" class="form-control" value="{{$bb->price}}">
+            <input name="price" id="txtPrice" class="form-control @error('price') is-invalid @enderror" value="{{$bb->price}}">
+            @error('price')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
         <input type="submit" class="btn btn-primary" value="Обновить">
     </form>
