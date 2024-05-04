28438e0a153d55daa4e22597f5710b9edc3dbdf8
Anton Grakhov <ant.grakhov@gmail.com>
2024-05-04 19:54:11 +0300

    Add price to detail page

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
 <h1>{{$item->title}}</h1>
+<h2>Описание</h2>
 <p>{{$item->content}}</p>
+<p>Цена: {{$item->price}}</p>
 <p><a href="{{route('index')}}">Вернуться назад</a></p>
 @endsection('content')
